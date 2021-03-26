a388210feb183299a7f718f59aaf84a78c4b73c5
Carlos Zuniga <czuniga@xtages.com>
2021-03-26 14:47:39 -0700

    Adds spring boot boilerplate
    Created using [1].
    
    The boilerplate includes:
    
    * devtools
    * webflux (to support reactive streams for web)
    * session (spring sessions)
    * postgresql
    * jooq (for typesafe SQL)
    * actuator (monitoring and management http endpoints)
    * liquibase (for DB migrations)
    
    I had to downgrade to JDK 15 (previously 16) because Gradle doesn't
    support version 16 yet.
    
    Neither test nor `gradle bootRun` work yet because liquibase expects
    a valid JDBC connection to work.
    
    [1] https://start.spring.io/#!type=gradle-project&language=kotlin&platformVersion=2.4.4.RELEASE&packaging=jar&jvmVersion=15&groupId=xtages&artifactId=console&name=console&description=Xtages%20Console%20Server&packageName=xtages.console&dependencies=devtools,webflux,session,postgresql,jooq,actuator,liquibase

---
+++ backend/src/test/kotlin/xtages/console/ConsoleApplicationTests.kt
@@ -0,0 +1,13 @@
+package xtages.console
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsoleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
