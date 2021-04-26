8bb675e5159236cce1b58d0cd91f33aa91ea0618
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-04-26 14:32:03 -0700

    maing build to build_events a 1:n

--- backend/src/main/resources/db/changelog/changesets/add-build.postgresql.sql
+++ backend/src/main/resources/db/changelog/changesets/add-build.postgresql.sql
@@ -3,22 +3,24 @@
 -- changeset mdellamerlina:3 logicalFilePath:xtages-console.xml
 CREATE TABLE "build_events"
 (
-	"id" SERIAL PRIMARY KEY,
-	"time" 	TIMESTAMPTZ NOT NULL,
-	"status" VARCHAR(255) NOT NULL,
-	"operation" VARCHAR(255) NOT NULL,
-	"user" INT NOT NULL,
-	"environment" VARCHAR(255) NOT NULL,
-	CONSTRAINT "build_events_xtages_user_id_fkey" FOREIGN KEY("user") REFERENCES xtages_user("id")
+    "id"            SERIAL PRIMARY KEY,
+    "build_id"      INT NOT NULL,
+    "time"          TIMESTAMPTZ NOT NULL,
+    "status"        VARCHAR(255) NOT NULL,
+    "operation"     VARCHAR(255) NOT NULL,
+    "user"          INT NOT NULL,
+    "environment"   VARCHAR(255) NOT NULL,
+    CONSTRAINT "build_events_xtages_user_id_fkey" FOREIGN KEY("user") REFERENCES xtages_user("id"),
+    CONSTRAINT "build_events_build_id_fkey" FOREIGN KEY("id") REFERENCES  build("id")
 )
 
 -- changeset mdellamerlina:4 logicalFilePath:xtages-console.xml
 CREATE TABLE "build"
 (
-	"project" 		VARCHAR(255) NOT NULL,
-	"commit" 		VARCHAR(255) NOT NULL,
-	"build_events" INT NOT NULL,
-	PRIMARY KEY("project", "commit"),
-	CONSTRAINT "build_project_name_fkey" FOREIGN KEY("project") REFERENCES project("name", "organization"),
-	CONSTRAINT "build_build_events_id_fkey" FOREIGN KEY("build_events") REFERENCES  build_events("id")
+    "id"            SERIAL UNIQUE,
+    "project"       VARCHAR(255) NOT NULL,
+    "commit"        VARCHAR(255) NOT NULL,
+    "build_events"  INT NOT NULL,
+    PRIMARY KEY("project", "commit"),
+    CONSTRAINT "build_project_name_fkey" FOREIGN KEY("project") REFERENCES project("name")
 )
--- backend/src/main/resources/db/changelog/changesets/add-project.postgresql.sql
+++ backend/src/main/resources/db/changelog/changesets/add-project.postgresql.sql
@@ -7,15 +7,16 @@ CREATE TYPE "project_type" AS ENUM
 -- changeset mdellamerlina:2 logicalFilePath:xtages-console.xml
 CREATE TABLE "project"
 (
-	"name" 		VARCHAR(255) NOT NULL UNIQUE,
-	"type" 		"project_type",
-	"version" 	INT,
-	"organization" INT NOT NULL,
-	"user" INT NOT NULL,
-	"pass_check_rule_enable" BOOLEAN NOT NULL DEFAULT FALSE,
-	PRIMARY KEY("name", "organization"),
-	CONSTRAINT "type_version_consistent" CHECK ((ROW ("type", "version") IS NULL) OR
+    "name"                      VARCHAR(255) NOT NULL,
+    "type"                      "project_type",
+    "version"                   INT,
+    "organization"              INT NOT NULL,
+    "user"                      INT NOT NULL,
+    "pass_check_rule_enable"    BOOLEAN NOT NULL DEFAULT FALSE,
+
+    PRIMARY KEY("name", "organization"),
+    CONSTRAINT "type_version_consistent" CHECK ((ROW ("type", "version") IS NULL) OR
                                                        (ROW ("type", "version") IS NOT NULL)),
-	CONSTRAINT "project_organization_name_fkey" FOREIGN KEY("name") REFERENCES organization("name"),
-	CONSTRAINT "project_xtages_user_id_fkey" FOREIGN KEY("user") REFERENCES xtages_user("id")
+    CONSTRAINT "project_organization_name_fkey" FOREIGN KEY("name") REFERENCES organization("name"),
+    CONSTRAINT "project_xtages_user_id_fkey" FOREIGN KEY("user") REFERENCES xtages_user("id")
 )
