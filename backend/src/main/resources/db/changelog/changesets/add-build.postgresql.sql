0046750d9168e347d68858cd7da3cf05e70e12b9
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-04-26 15:57:38 -0700

    Db tables creation (#34)
    * DB tables for project and build
    
    * adding environment to buildevents table
    
    * removing xtages prefix from tables
    
    * rename foreing keys
    
    * adding unquiness to build project name
    
    * maing build to build_events a 1:n
    
    * adding project id to project
    
    * deleting build_events column in build table

---
+++ backend/src/main/resources/db/changelog/changesets/add-build.postgresql.sql
@@ -0,0 +1,25 @@
+-- liquibase formatted sql
+
+-- changeset mdellamerlina:3 logicalFilePath:xtages-console.xml
+CREATE TABLE "build"
+(
+    "id"            SERIAL UNIQUE,
+    "project_id"    INT NOT NULL,
+    "commit"        VARCHAR(255) NOT NULL,
+    PRIMARY KEY("project_id", "commit"),
+    CONSTRAINT "build_project_id_fkey" FOREIGN KEY("project_id") REFERENCES project("id")
+)
+
+-- changeset mdellamerlina:4 logicalFilePath:xtages-console.xml
+CREATE TABLE "build_events"
+(
+    "id"            SERIAL PRIMARY KEY,
+    "build_id"      INT NOT NULL,
+    "time"          TIMESTAMPTZ NOT NULL,
+    "status"        VARCHAR(255) NOT NULL,
+    "operation"     VARCHAR(255) NOT NULL,
+    "user"          INT NOT NULL,
+    "environment"   VARCHAR(255) NOT NULL,
+    CONSTRAINT "build_events_xtages_user_id_fkey" FOREIGN KEY("user") REFERENCES xtages_user("id"),
+    CONSTRAINT "build_events_build_id_fkey" FOREIGN KEY("id") REFERENCES  build("id")
+)
---
+++ backend/src/main/resources/db/changelog/changesets/add-project.postgresql.sql
@@ -0,0 +1,23 @@
+-- liquibase formatted sql
+
+-- changeset mdellamerlina:1 logicalFilePath:xtages-console.xml
+CREATE TYPE "project_type" AS ENUM
+    ('NODEJS');
+
+-- changeset mdellamerlina:2 logicalFilePath:xtages-console.xml
+CREATE TABLE "project"
+(
+    "id"                        SERIAL  PRIMARY KEY,
+    "name"                      VARCHAR(255) NOT NULL,
+    "type"                      "project_type",
+    "version"                   INT,
+    "organization"              INT NOT NULL,
+    "user"                      INT NOT NULL,
+    "pass_check_rule_enable"    BOOLEAN NOT NULL DEFAULT FALSE,
+
+    UNIQUE(name, organization),
+    CONSTRAINT "type_version_consistent" CHECK ((ROW ("type", "version") IS NULL) OR
+                                                       (ROW ("type", "version") IS NOT NULL)),
+    CONSTRAINT "project_organization_name_fkey" FOREIGN KEY("name") REFERENCES organization("name"),
+    CONSTRAINT "project_xtages_user_id_fkey" FOREIGN KEY("user") REFERENCES xtages_user("id")
+)
