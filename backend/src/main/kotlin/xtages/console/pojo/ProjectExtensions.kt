9c62f1409de6d88975be3019e9a8372ea26c79f7
Carlos Zuniga <czuniga@xtages.com>
2021-04-28 12:24:02 -0700

    Create ECR repo and CodeBuild CI project

--- backend/src/main/kotlin/xtages/console/pojo/ProjectExtensions.kt
+++ backend/src/main/kotlin/xtages/console/pojo/ProjectExtensions.kt
@@ -130,3 +130,30 @@ val Project.codeBuildCdLogsStreamName: String
         val name = ensure.notNull(value = name, valueDesc = "project.name")
         return "${name}_cd_logs"
     }
+
+val Project.codeBuildCiProjectName: String
+    get() {
+        val name = ensure.notNull(value = name, valueDesc = "project.name")
+        val organization = ensure.notNull(value = organization, valueDesc = "project.organization")
+        return "${organization}__${name}__ci"
+    }
+
+
+val Project.codeBuildCiImageName: String
+    get() {
+        val type = ensure.notNull(value = type, valueDesc = "project.type")
+        val version = ensure.notNull(value = version, valueDesc = "project.version")
+        return "${type}_${version}_ci"
+    }
+
+val Project.codeBuildCiLogsGroupName: String
+    get() {
+        val organization = ensure.notNull(value = organization, valueDesc = "project.organization")
+        return "${organization}_ci_logs"
+    }
+
+val Project.codeBuildCiLogsStreamName: String
+    get() {
+        val name = ensure.notNull(value = name, valueDesc = "project.name")
+        return "${name}_ci_logs"
+    }
