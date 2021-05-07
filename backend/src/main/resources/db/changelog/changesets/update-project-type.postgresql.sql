1256b4132873cf0d8dd4fbaabead921259b5102a
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-07 12:21:21 -0700

    renaming buildspec file in s3 and update db

--- backend/src/main/resources/db/changelog/changesets/update-project-type.postgresql.sql
+++ backend/src/main/resources/db/changelog/changesets/update-project-type.postgresql.sql
@@ -2,3 +2,5 @@
 
 -- changeset mdellamerlina:6 logicalFilePath:xtages-console.xml
 ALTER TYPE project_type RENAME VALUE 'NODEJS' TO 'NODE';
+
+UPDATE "project" SET "version" = '15.13.0' WHERE "version" = '15'
