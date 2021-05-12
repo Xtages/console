ac3bc498aa239f220735bdcdf8df91571532ba99
Carlos Zuniga <czuniga@xtages.com>
2021-05-12 11:42:48 -0700

    Parses CodeBuild events into data classes (#56)

--- backend/src/main/kotlin/xtages/console/config/AwsClientConfig.kt
+++ backend/src/main/kotlin/xtages/console/config/AwsClientConfig.kt
@@ -1,7 +1,14 @@
 package xtages.console.config
 
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.awspring.cloud.messaging.config.QueueMessageHandlerFactory
+import io.awspring.cloud.messaging.core.QueueMessagingTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.converter.MappingJackson2MessageConverter
+import org.springframework.messaging.converter.MessageConverter
+import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider
 import software.amazon.awssdk.services.codebuild.CodeBuildAsyncClient
 import software.amazon.awssdk.services.codestarnotifications.CodestarNotificationsAsyncClient
@@ -26,4 +33,26 @@ class AwsClientConfig {
 
     @Bean
     fun codestarNotificationsClient(): CodestarNotificationsAsyncClient = CodestarNotificationsAsyncClient.create()
+
+    @Bean
+    fun queueMessagingTemplate(amazonSQSAsync: AmazonSQSAsync): QueueMessagingTemplate =
+        QueueMessagingTemplate(amazonSQSAsync)
+
+    @Bean
+    fun queueMessageHandlerFactory(mapper: ObjectMapper, amazonSQSAsync: AmazonSQSAsync): QueueMessageHandlerFactory {
+        val queueHandlerFactory = QueueMessageHandlerFactory()
+        queueHandlerFactory.setAmazonSqs(amazonSQSAsync)
+        queueHandlerFactory.setArgumentResolvers(
+            listOf(PayloadMethodArgumentResolver(jackson2MessageConverter(mapper)))
+        )
+        return queueHandlerFactory
+    }
+
+    private fun jackson2MessageConverter(mapper: ObjectMapper): MessageConverter {
+        val converter = MappingJackson2MessageConverter()
+        // set strict content type match to false to enable the listener to handle AWS events
+        converter.isStrictContentTypeMatch = false
+        converter.objectMapper = mapper
+        return converter
+    }
 }
