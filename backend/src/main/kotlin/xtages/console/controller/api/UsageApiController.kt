59c7899b8763e23962b46dffb5c1c2a9e3bad817
Carlos Zuniga <czuniga@xtages.com>
2021-06-21 16:01:01 -0700

    Adds an API to retrieve usage details for a resource (or all) (#106)

---
+++ backend/src/main/kotlin/xtages/console/controller/api/UsageApiController.kt
@@ -0,0 +1,55 @@
+package xtages.console.controller.api
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import xtages.console.controller.api.model.ResourceType
+import xtages.console.controller.api.model.UsageDetail
+import xtages.console.controller.model.usageDetailPojoToUsageDetail
+import xtages.console.dao.fetchOneByCognitoUserId
+import xtages.console.query.tables.daos.OrganizationDao
+import xtages.console.query.tables.pojos.Organization
+import xtages.console.service.AuthenticationService
+import xtages.console.service.UsageService
+import java.util.concurrent.CompletableFuture
+import java.util.stream.Stream
+import kotlin.streams.toList
+import xtages.console.query.enums.ResourceType as ResourceTypePojo
+import xtages.console.service.UsageDetail as UsageDetailPojo
+
+/**
+ * Controller to handle queries for usage of resources.
+ */
+@Controller
+class UsageApiController(
+    val organizationDao: OrganizationDao,
+    val usageService: UsageService,
+    val authenticationService: AuthenticationService,
+) : UsageApiControllerBase {
+
+    override fun getAllUsageDetails(): ResponseEntity<List<UsageDetail>> {
+        val organization = organizationDao.fetchOneByCognitoUserId(authenticationService.currentCognitoUserId)
+        val usageFutures = ResourceType.values().map { resourceType ->
+            CompletableFuture.supplyAsync {
+                getUsageDetail(organization, resourceType = resourceType)
+            }
+        }
+        val usages = Stream.of(*usageFutures.toTypedArray())
+            .map { future -> future.join() }
+            .toList()
+            .mapNotNull(usageDetailPojoToUsageDetail::convert)
+        return ResponseEntity.ok(usages)
+    }
+
+    override fun getUsageDetail(resourceType: ResourceType): ResponseEntity<UsageDetail> {
+        val organization = organizationDao.fetchOneByCognitoUserId(authenticationService.currentCognitoUserId)
+        val usageDetail = getUsageDetail(organization, resourceType)
+        return ResponseEntity.ok(usageDetailPojoToUsageDetail.convert(usageDetail))
+    }
+
+    private fun getUsageDetail(organization: Organization, resourceType: ResourceType): UsageDetailPojo {
+        return usageService.getUsageDetail(
+            organization,
+            resourceType = ResourceTypePojo.valueOf(resourceType.value)
+        )
+    }
+}
