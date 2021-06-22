ba4aa222a47c9c721b684046fb83059a8425d45b
Carlos Zuniga <czuniga@xtages.com>
2021-06-22 10:40:26 -0700

    Implements the usage section of the account page. (#107)
    The Usage section of the account page is composed of UsageCards each
    one for a ResourceType.
    
    This PR also adds a better error message when trying to create a new
    project and the user hits the limit.
    
    Finally this PR introduces LoadIndicatingSection which is a Section that
    is backed by data that is feched by useQuery. This component will
    consistently apply a spinner (when the data is being loaded) and show
    an error message if the data fails to load.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,32 +1,18 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
 import {useQuery} from 'react-query';
 import {projectApi} from 'service/Services';
 import {Codesandbox, Copy} from 'react-feather';
 import {ProjectTable} from 'components/project/ProjectTable';
 import ProjectTemplateCard from 'components/project/ProjectTemplateCard';
-import {Section, SectionTitle} from 'components/layout/Section';
+import {LoadIndicatingSection, Section, SectionTitle} from 'components/layout/Section';
 import Page from 'components/layout/Page';
 import {useHistory} from 'react-router-dom';
 
 export default function HomePage() {
-  const {
-    isLoading,
-    error,
-    data,
-  } = useQuery(
+  const getProjectsQueryResult = useQuery(
     'projects',
     () => projectApi.getProjects(true),
   );
-  let projectTable: string | ReactNode;
-  if (isLoading) {
-    projectTable = 'Loading...';
-  } else if (error) {
-    projectTable = `An error has occurred: ${error}`;
-  } else if (data?.data != null) {
-    projectTable = (
-      <ProjectTable projects={data.data} />
-    );
-  }
   const history = useHistory();
   return (
     <Page>
@@ -42,12 +28,16 @@ export default function HomePage() {
           />
         </div>
       </Section>
-      <Section last>
-        <SectionTitle icon={Codesandbox} title="Projects" subtitle="Manage all your projects" />
-        <div className="col-12">
-          {projectTable}
-        </div>
-      </Section>
+      <LoadIndicatingSection queryResult={getProjectsQueryResult} last>
+        {(axiosResponse) => (
+          <>
+            <SectionTitle icon={Codesandbox} title="Projects" subtitle="Manage all your projects" />
+            <div className="col-12">
+              <ProjectTable projects={axiosResponse.data} />
+            </div>
+          </>
+        )}
+      </LoadIndicatingSection>
     </Page>
   );
 }
