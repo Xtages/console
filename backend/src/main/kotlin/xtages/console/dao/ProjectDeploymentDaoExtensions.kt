3676c924556d85026bdf4fa0462088b7cae89382
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-07-28 14:16:53 -0700

    Updating the build information (#138)
    * Updating the build information
    This will update the deploy information when a deploy / undeploy happens
    
    * Listen to Cloudtrail event for ECS
    
    * Listening to UpdateService for deployment
    When there is a deployment that is not first one that created the
    service the ECS event `SERVICE_DEPLOYMENT_COMPLETED` is not triggered.
    This adds a CloudTrail event to listen when the service is updated with
    a desiredCount = 1. It's still not fullproof because we are listening to
    when the service is redeploy but not when the service is in a steady
    state. Still, it's better than nothing now.
    
    * Quite some changes
    - adding project deployment status
    - fixing some warnings for Int64 in openAPI definition
    - Extracting environment type
    - Still pending the deploy status for the project in the Deployment
      object
    
    * Updating queries and deployment object
    This updates the deployment object to have the project deployment
    status.
    I have also updated the query for `fetchLatestDeploymentStatus` to
    eliminate the subquery
    
    * fixing query to get latest project deployment
    
    * addressing review comments

---
+++ backend/src/main/kotlin/xtages/console/dao/ProjectDeploymentDaoExtensions.kt
@@ -0,0 +1,58 @@
+package xtages.console.dao
+
+import org.jooq.impl.DSL.max
+import xtages.console.controller.model.Environment
+import xtages.console.query.tables.daos.ProjectDeploymentDao
+import xtages.console.query.tables.pojos.Build
+import xtages.console.query.tables.pojos.ProjectDeployment
+import xtages.console.query.tables.references.BUILD
+import xtages.console.query.tables.references.PROJECT
+import xtages.console.query.tables.references.PROJECT_DEPLOYMENT
+import java.time.LocalDateTime
+
+/***
+ * Returns the lastest [ProjectDeployment] based on the [projectHash]
+ * and the [environment]
+ */
+fun ProjectDeploymentDao.fetchLatestDeploymentStatus(
+    projectHash: String,
+    environment: Environment
+): ProjectDeployment? {
+    return ctx()
+        .select(PROJECT_DEPLOYMENT.asterisk())
+        .from(PROJECT_DEPLOYMENT)
+        .join(PROJECT).on(PROJECT_DEPLOYMENT.PROJECT_ID.eq(PROJECT.ID))
+        .join(BUILD).on(PROJECT_DEPLOYMENT.BUILD_ID.eq(BUILD.ID))
+        .where(
+            PROJECT.HASH.eq(projectHash)
+                .and(BUILD.ENVIRONMENT.eq(environment.name.toLowerCase()))
+        )
+        .orderBy(PROJECT_DEPLOYMENT.START_TIME.desc())
+        .limit(1)
+        .fetchOneInto(ProjectDeployment::class.java)
+}
+
+/***
+ * Returns the latest [ProjectDeployment] based on the [builds] list
+ */
+fun ProjectDeploymentDao.fetchLatestByBuilds(builds: List<Build>): List<ProjectDeployment> {
+    val latestDeployments = ctx()
+        .select(
+            PROJECT_DEPLOYMENT.BUILD_ID,
+            max(PROJECT_DEPLOYMENT.START_TIME).`as`("latest_time")
+        )
+        .from(PROJECT_DEPLOYMENT)
+        .where(PROJECT_DEPLOYMENT.BUILD_ID.`in`(builds.map { it.id }))
+        .groupBy(PROJECT_DEPLOYMENT.BUILD_ID).asTable("latest_build")
+    return ctx()
+        .select(
+            PROJECT_DEPLOYMENT.asterisk()
+        )
+        .from(
+            latestDeployments
+        )
+        .join(PROJECT_DEPLOYMENT)
+        .on(PROJECT_DEPLOYMENT.START_TIME.eq(latestDeployments.field("latest_time", LocalDateTime::class.java)))
+        .fetchInto(ProjectDeployment::class.java)
+
+}
