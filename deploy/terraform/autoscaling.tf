67d562bc8389aa3ee2531e19ddce0c20e70d9b41
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-08-13 16:38:36 -0700

    ECS Autoscaling for console based on CPU utlization

---
+++ deploy/terraform/autoscaling.tf
@@ -0,0 +1,27 @@
+resource "aws_appautoscaling_target" "ecs_console_autoscaling" {
+  max_capacity       = 20
+  min_capacity       = 1
+  resource_id        = "service/${local.ecs_cluster_name}/${aws_ecs_service.xtages_console_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "console-scale-out"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_console_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_console_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_console_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 80
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 120
+  }
+}
+
