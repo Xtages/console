341a5d47e14e31b616bdfc67cd9940d348412ede
Carlos Zuniga <czuniga@xtages.com>
2021-08-18 15:25:39 -0700

    Partial Revert "A couple of quality of life improvements (#161)" (#172)
    Also logs the caller identity when the app starts.
    
    This reverts commit e094491c174f1f5d60a6a8baefb6322409928a93.

--- backend/src/main/kotlin/xtages/console/ConsoleApplication.kt
+++ backend/src/main/kotlin/xtages/console/ConsoleApplication.kt
@@ -1,10 +1,18 @@
 package xtages.console
 
+import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.stereotype.Component
+import software.amazon.awssdk.services.sts.StsAsyncClient
+import software.amazon.awssdk.services.sts.model.GetCallerIdentityRequest
 import xtages.console.config.ConsoleProperties
+
+private val logger = KotlinLogging.logger { }
 
 @EnableAsync
 @SpringBootApplication
@@ -15,4 +23,20 @@ fun main(args: Array<String>) {
     // Disable the JOOQ logo from logs
     System.getProperties().setProperty("org.jooq.no-logo", "true")
     runApplication<ConsoleApplication>(*args)
+}
+
+@Component
+private class CallerIdentityLogger(private val stsClient: StsAsyncClient) {
+    @EventListener
+    fun onApplicationEvent(event: ContextRefreshedEvent) {
+        val callerIdentityResponse = stsClient.getCallerIdentity(GetCallerIdentityRequest.builder().build()).get()
+        logger.info {
+            """
+
+AWS UserId: ${callerIdentityResponse.userId()}
+AWS Account: ${callerIdentityResponse.account()}
+AWS Arn: ${callerIdentityResponse.arn()}
+"""
+        }
+    }
 }
