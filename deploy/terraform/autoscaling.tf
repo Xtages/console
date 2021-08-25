12578f110f3a1cdc0c38768310a6707da63e94c2
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-08-25 10:18:17 -0700

    Adding autoscaling based on memory utilization (#185)

--- deploy/terraform/autoscaling.tf
+++ deploy/terraform/autoscaling.tf
@@ -25,3 +25,22 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
   }
 }
 
+resource "aws_appautoscaling_policy" "console_ecs_policy_memory" {
+  name               = "console-scale-out-memory"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_console_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_console_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_console_autoscaling.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 80
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 120
+  }
+}
+
