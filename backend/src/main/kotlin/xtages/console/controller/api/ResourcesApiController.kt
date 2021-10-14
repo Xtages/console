52fb14c44d17fb351316a7fcf54b5f4ac6741790
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-10-14 11:38:57 -0700

    Endpoint to request a DB (#216)
    * Endpoint to request a DB
    
    * mostly format changes

---
+++ backend/src/main/kotlin/xtages/console/controller/api/ResourcesApiController.kt
@@ -0,0 +1,40 @@
+package xtages.console.controller.api
+
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import xtages.console.controller.api.model.Resource
+import xtages.console.dao.fetchLatestPlan
+import xtages.console.dao.maybeFetchOneByCognitoUserId
+import xtages.console.exception.UserNeedsToHavePlanException
+import xtages.console.query.tables.daos.OrganizationDao
+import xtages.console.query.tables.daos.OrganizationToPlanDao
+import xtages.console.service.AuthenticationService
+import xtages.console.service.aws.RdsService
+
+class ResourcesApiController(
+    private val rdsService: RdsService,
+    private val organizationDao: OrganizationDao,
+    private val authenticationService: AuthenticationService,
+    private val organizationToPlanDao: OrganizationToPlanDao,
+) : ResourcesApiControllerBase {
+
+    override fun provisionResource(
+        @PathVariable("resource") resource: Resource
+    ): ResponseEntity<Unit> {
+        val organization = organizationDao.maybeFetchOneByCognitoUserId(authenticationService.currentCognitoUserId)
+        organization ?: run {
+            val plan = organizationToPlanDao.fetchLatestPlan(organization!!)
+                ?: throw UserNeedsToHavePlanException(
+                    "User needs to have a plan associated to provision a DB"
+                )
+            when (resource) {
+                Resource.DB -> {
+                    rdsService.provisionDb(organization, plan)
+                }
+            }
+            return ResponseEntity.ok().build()
+        }
+        return ResponseEntity(FORBIDDEN)
+    }
+}
