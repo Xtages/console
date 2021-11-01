9af75a60533968ead3c50d06d5d82f95b008ea59
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-11-01 09:38:17 -0700

    changing db instance & new recipe version (#244)
    DB instance for paying customers will bd db.t4g.medium as it's cheaper
    and same performance than t3t

---
+++ backend/src/main/resources/db/changelog/changesets/00014-changing-db-instance.postgresql.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset mdellamerlina:69 logicalFilePath:xtages-console.xml
+UPDATE "plan"
+SET "db_instance" = 'db.t4g.medium'
+WHERE "paid" = true;
+
+INSERT INTO public.recipe(
+	project_type, version, repository, tag, build_script_path, deploy_script_path, promote_script_path, rollback_script_path)
+	VALUES ('NODE', '15.13.0', 'Xtages/recipes', 'v0.1.25', 'node/ci/build.sh', 'node/cd/deploy.sh', 'node/cd/promote.sh', 'node/cd/rollback.sh');
